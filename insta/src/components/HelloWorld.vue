be588a1682de71318f6cff5acbdd980e5af5e1b3
Valeria Hz <valeriahz.137@gmail.com>
2024-03-04 18:35:20 -0800

    first commit

---
+++ insta/src/components/HelloWorld.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="gral">
+    <div class="logo">
+      <img src="./../assets/logo.png">
+    </div>
+     <div class="cora">
+      <img src="./../assets/corazon.png">
+     </div>  
+     <div class="mensaje">
+      <img src="./../assets/mensaje.png">
+     </div>
+  </div>
+  <div class="histo">
+   <div class="historias">
+    <div class="foto">
+    </div>
+   </div>
+   <div class="historias">
+    <div class="foto">
+    </div>
+   </div>
+   <div class="historias">
+    <div class="foto">
+    </div>
+   </div>
+   <div class="historias">
+    <div class="foto">
+    </div>
+   </div>
+   <div class="historias">
+    <div class="foto">
+    </div>
+   </div>
+   <div class="historias">
+    <div class="foto">
+    </div>
+   </div>
+   <div class="historias">
+    <div class="foto">
+    </div>
+   </div>
+   <div class="historias">
+    <div class="foto">
+    </div>
+   </div>
+   <div class="historias">
+    <div class="foto">
+    </div>
+   </div>
+   <div class="historias">
+    <div class="foto">
+    </div>
+   </div>
+   <div class="historias">
+    <div class="foto">
+    </div>
+   </div>
+ </div>
+ <div class="post">
+  <div class="usuario">
+    bjdjbf
+  </div>
+  <div class="imagen">
+aquiu va el post
+  </div>
+  <div class="reacion">
+    awuoi va las reacciones
+  </div>
+  <div class="comen">
+    aqui van los comentarios 
+  </div>
+
+ </div>
+</template>
+
+
