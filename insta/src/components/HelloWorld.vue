c53d36c93d3298c2a7e0660a1402ed825d944605
Valeria Hz <valeriahz.137@gmail.com>
2024-03-09 22:14:56 -0800

    insta

--- insta/src/components/HelloWorld.vue
+++ insta/src/components/HelloWorld.vue
@@ -13,62 +13,127 @@
   <div class="histo">
    <div class="historias">
     <div class="foto">
+      <img src="./../assets/hisuno.png">
+      <div class="us">
+        Jesen
+      </div>
     </div>
    </div>
    <div class="historias">
     <div class="foto">
+      <img src="./../assets/hisdos.png">
+      <div class="us">
+        Vladimir
+      </div>
     </div>
    </div>
    <div class="historias">
     <div class="foto">
+      <img src="./../assets/histres.png">
+      <div class="us">
+        Diana
+      </div>
     </div>
    </div>
    <div class="historias">
     <div class="foto">
+      <img src="./../assets/hiscuatro.png">
+      <div class="us">
+        Tobias
+      </div>
     </div>
    </div>
    <div class="historias">
     <div class="foto">
+      <img src="./../assets/imagen.jpg">
+      <div class="us">
+        Villa
+      </div>
     </div>
    </div>
    <div class="historias">
     <div class="foto">
+      <img src="./../assets/hiscinco.png">
+      <div class="us">
+        Mario
+      </div>
     </div>
    </div>
    <div class="historias">
     <div class="foto">
+      <img src="./../assets/hisseis.png">
+      <div class="us">
+        Evelio
+      </div>
     </div>
    </div>
    <div class="historias">
     <div class="foto">
+      <img src="./../assets/hissiete.png">
+      <div class="us">
+        Coco
+      </div>
     </div>
    </div>
    <div class="historias">
     <div class="foto">
+      <img src="./../assets/hisocho.png">
+      <div class="us">
+        Arturo
+      </div>
     </div>
    </div>
    <div class="historias">
     <div class="foto">
+      <img src="./../assets/hisnueve.png">
+      <div class="us">
+        Chems
+      </div>
     </div>
    </div>
    <div class="historias">
     <div class="foto">
+      <img src="./../assets/hisdiez.png">
+      <div class="us">
+        Flork
+      </div>
     </div>
    </div>
- </div>
+  </div>
+
  <div class="post">
   <div class="usuario">
-    bjdjbf
+    <img src="./../assets/imagen.jpg">
+    <div>Valehzg_</div>
   </div>
-  <div class="imagen">
-aquiu va el post
-  </div>
+
+ <div class="imagen">
+  <img src="./../assets/imagen.jpg">
+ </div>
   <div class="reacion">
-    awuoi va las reacciones
+    <div class="reac">
+    <img src="./../assets/corazon.png">
+    <img src="./../assets/coment.png">
+    <img src="./../assets/enviar.png">
+    </div>
+    <div class="guardar">
+      <img src="./../assets/guardar.png"> 
+    </div>
   </div>
-  <div class="comen">
-    aqui van los comentarios 
+  <div class="caja">
+  <div class="likes">
+    14,812
   </div>
+  <div class="descripcion">
+    Valehzg_ :D
+  </div>
+  <div class="comentarios">
+    Ver los 6 comentarios
+  </div>
+  <div class="fecha">
+    9 enero
+  </div>
+</div>
 
  </div>
 </template>
